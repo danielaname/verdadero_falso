b77efd4255e98e97aeb73419abdee932701b7c67
codeamac12 <codeamac12@gmail.com>
2017-04-19 19:52:44 -0500

    version 1 de ejercicio 1

---
+++ verdadero_falso.rb
@@ -0,0 +1,12 @@
+p igual_que = 50 == 50
+p #=> true
+p menor_que = 23 < 30
+p #=> true
+p mayor_que = 54 > 40
+p #=> true
+p menor_o_igual_que = 45 <= 45
+p #=> true
+p mayor_o_igual_que = 56 >= 45
+p #=> true
+p diferente_que = 34 != 34
+p #=> falso
